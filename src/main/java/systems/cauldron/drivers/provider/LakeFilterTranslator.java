4e40ae49ed7004ef91743f1c473920e87c58a35d
Amann Malik <amannmalik@gmail.com>
2018-08-26 09:52:54 -0500

    cleanup

--- src/main/java/systems/cauldron/drivers/provider/LakeFilterTranslator.java
+++
@@ -1,9 +0,0 @@
-package systems.cauldron.drivers.provider;
-
-import org.apache.calcite.rex.RexNode;
-
-import java.util.List;
-
-public interface LakeFilterTranslator {
-    String compileQuery(List<RexNode> filters, int[] projects);
-}
