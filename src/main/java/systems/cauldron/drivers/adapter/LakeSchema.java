5e3f6ac88707a316ae5638ed48c5264438ead993
Amann Malik <amannmalik@gmail.com>
2018-08-26 10:28:56 -0500

    remove unused

--- src/main/java/systems/cauldron/drivers/adapter/LakeSchema.java
+++ src/main/java/systems/cauldron/drivers/adapter/LakeSchema.java
@@ -1,6 +1,5 @@
 package systems.cauldron.drivers.adapter;
 
-import org.apache.calcite.rel.type.RelProtoDataType;
 import org.apache.calcite.schema.Table;
 import org.apache.calcite.schema.impl.AbstractSchema;
 
@@ -11,13 +10,10 @@ import java.util.stream.Collectors;
 public class LakeSchema extends AbstractSchema {
 
     private Map<String, Table> tableMap;
-    private Map<String, RelProtoDataType> typeMap;
 
     public LakeSchema(List<LakeTable> tables) {
         super();
         this.tableMap = tables.stream().collect(Collectors.toMap(LakeTable::getLabel, t -> t));
-        this.typeMap = tableMap.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey,
-                e -> (RelProtoDataType) relDataTypeFactory -> e.getValue().getRowType(relDataTypeFactory)));
     }
 
     @Override
@@ -28,10 +24,5 @@ public class LakeSchema extends AbstractSchema {
     @Override
     protected Map<String, Table> getTableMap() {
         return tableMap;
-    }
-
-    @Override
-    protected Map<String, RelProtoDataType> getTypeMap() {
-        return typeMap;
     }
 }
