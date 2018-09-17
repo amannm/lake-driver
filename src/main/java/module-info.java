b39d2642594821be4333197985569ba3a9ab578c
Amann Malik <amannmalik@gmail.com>
2018-09-16 20:15:18 -0500

    format like a begrudging wiring migration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,24 +1,12 @@
 module systems.cauldron.drivers.lake {
-
-    //logging
+    exports systems.cauldron.drivers.lake;
     requires slf4j.api;
     requires slf4j.simple;
-
-    //SQL engine
     requires java.sql;
     requires calcite.core;
     requires calcite.linq4j;
-
-    //JSON tools
     requires java.json;
-
-    //flat-file parsing
     requires univocity.parsers;
-
-    //cloud storage
     requires sdk.core;
     requires s3;
-
-    exports systems.cauldron.drivers.lake;
-
 }
