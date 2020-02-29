d7a5657412dbe7dcc1781d971eb4ffc78377801b
Amann Malik <amannmalik@gmail.com>
2020-02-29 14:19:17 -0600

    clear out temporarily

--- src/main/java/systems/cauldron/drivers/lake/parser/RecordParser.java
+++
@@ -1,8 +0,0 @@
-package systems.cauldron.drivers.lake.parser;
-
-import java.io.Closeable;
-import java.util.Optional;
-
-public interface RecordParser extends Closeable {
-    Optional<Object[]> parseRecord();
-}
